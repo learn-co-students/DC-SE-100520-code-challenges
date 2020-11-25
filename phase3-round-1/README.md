1db37cce4b2c094d5664a17241b00dd7609fa7c4
JakesProjects1 <jacob@kagon.net>
2020-11-25 08:32:35 -0800

    updates

--- phase3-round-1/README.md
+++ phase3-round-1/README.md
@@ -29,8 +29,8 @@ The endpoints you will need are:
 
 As a user, I can:
 
-- See the image received from the server, including its title, likes and comments when the page loads
-- Click on the heart icon to increase image likes, and still see them when I reload the page
+<!-- - See the image received from the server, including its title, likes and comments when the page loads
+- Click on the heart icon to increase image likes, and still see them when I reload the page -->
 - Add a comment (no persistance needed)
 
 ## Advanced Deliverables
--- phase3-round-1/src/index.js
+++ phase3-round-1/src/index.js
@@ -1,1 +1,93 @@
 // write your code here
+const imgURL = 'http://localhost:3000/images'
+const commentsURL ='http://localhost:3000/comments/'
+const counter = 1
+const counterTwo = 2
+const counterThree = 3
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    getImages()
+    getComments()
+})
+
+function getImages() {
+    fetch(`${imgURL}/1`)
+    .then(resp => resp.json())
+    .then(img => renderImg(img))
+}
+
+function getComments() {
+    fetch(`${commentsURL}/1`)
+    .then(resp => resp.json())
+    .then(comment => renderComments(comment))
+}
+
+
+
+function renderImg(img) {
+    let imgContainer = document.querySelector('.image-container')
+    let h2 = document.querySelector('.title')
+    h2.innerText = img.title
+    let image = document.querySelector('.image')
+    image.src = img.image
+    let likes = document.querySelector('.likes')
+    likes.innerText = `${img.likes} likes`
+    let button = document.querySelector(".like-button")
+    button.addEventListener('click', () => {
+        addLikes(likes, img)
+    })
+
+ let submit_comment =  document.querySelector(".comment-form")
+ submit_comment.addEventListener('submit', (event) => {
+     createComment(img ,event)
+ })
+}
+
+function renderComments(comment) {
+    let commentList = document.querySelector(".comments")
+    
+    let newListItem = document.createElement('li')
+    newListItem.innerText = comment.content
+    let firstList = document.getElementsByTagName('li')[0]
+    firstList.innerText = comment.content
+    commentList.append(newListItem)
+}
+
+function createComment(img, event) {
+    event.preventDefault()
+
+    let newComment = {}
+    newComment.imageId = img.id,
+    newComment.content = event.target.comment.value
+
+    fetch(commentsURL, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newComment)
+    }).then(resp => resp.json)
+    .then(comment => renderComments(comment))
+
+}
+
+
+
+
+
+function addLikes(likes, img) {
+    let id = img.id
+    let newLikes = +likes.innerText.split(' ')[0] + 1
+
+   
+   let data = {}
+   data.likes = newLikes
+
+   fetch(`${imgURL}/${id}`, {
+       method: "PATCH",
+       headers: {"Content-Type": "application/json"},
+       body: JSON.stringify(data)
+   }).then(resp => resp.json())
+   .then(newData => renderImg(newData))
+
+}
