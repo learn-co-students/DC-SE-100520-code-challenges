20d34d377e9a00b8074b2dc32631acc19e7c80ce
chirptotheeast <alyssa.easterly@yahoo.com>
2020-11-25 12:30:03 -0500

    final commit

--- phase3-round-1/src/index.js
+++ phase3-round-1/src/index.js
@@ -16,7 +16,6 @@ function getImage(){
 }
 
 function renderImage(image){
-  
     let imageCard = document.getElementById('imagecard')
     
     let h2 = document.getElementById('h2-title')
@@ -37,8 +36,11 @@ function renderImage(image){
     })
    
 }
-
+ // having an issue with my updateLikes function, it updates the DB, 
+//and the likes on the page, but you currently have to 
+//refresh the page to see the new likes number
 function updateLikes(image){
+    
      let imageId =image[0].id
      let addedLikes =image[0].likes + 1
 
@@ -64,4 +66,35 @@ function getComments(){
 function renderComments(comment){
    let commentBox = document.getElementById('comment-id')
     commentBox.innerText = comment[0].content
+
+    let commentButton = document.getElementById('commenter-button')
+   // console.log(commentButton)
+
+   commentButton.addEventListener('click', (event) => {
+      addComment(comment)
+   })
+}
+
+function addComment(event){
+    event.preventDefault()
+
+    //let commentInput = document.getElementById('comment-inputer')
+    
+    let newCommentData = {
+        //imageId: 1,
+        content: event.target.content.value
+        //having issue getting the new comments to POST to the DB, 
+        //I think it might have something to do with line 85 
+        //maybe its not quite targetting the right input data
+        //OR it has something to do with this input being a part of 
+        //a form. I'm a little unclear.
+    }
+   
+    fetch(commentUrl, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(newCommentData)
+    }) .then (res => res.json())
+       .then (json => renderComments(comment))
+    
 }
