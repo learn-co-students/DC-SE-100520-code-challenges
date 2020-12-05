cefa0288cd37f7905b3bca17b75fdc1e2d07875f
BHessel <hesselfitness@gmail.com>
2020-12-05 11:04:16 -0500

    deliverable 1 done

--- Phase-3-Round-2/Flatabeer/src/index.js
+++ Phase-3-Round-2/Flatabeer/src/index.js
@@ -1,2 +1,37 @@
-// Code here
-console.log('hello')
+const url = 'http://localhost:3000/beers/1'
+
+//ON PAGE LOAD
+document.addEventListener('DOMContentLoaded', () => {
+    //see FIRST beer's details
+    fetch(url)
+        .then(res => res.json())
+        .then(beer => renderBeer(beer))
+})
+
+
+function renderBeer(beer){
+    //include name
+    let beerName = document.querySelector('#beerName')
+    beerName.innerText = beer.name
+
+    //image
+    let beerPic = document.querySelector('#beerPic')
+    beerPic.src = beer.image_url
+
+    //descrip
+    let beerFacts = document.querySelector('#descrip')
+    beerFacts.innerText = beer.description
+
+    //reviews
+    let beerReviews = document.querySelector('.reviews')
+    ReviewArray = beer.reviews
+
+    ReviewArray.forEach(review => {
+        let li = document.createElement('li')
+        li.innerText = review
+        beerReviews.appendChild(li)
+    })
+
+
+
+}
