3276bcfa778fecd709f4483f97086432e18db83a
JakesProjects1 <jacob@kagon.net>
2020-12-18 17:27:38 -0500

    passed down state as props

--- BotBattlr/src/containers/BotsPage.js
+++ BotBattlr/src/containers/BotsPage.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import BotCollection from './BotCollection'
 
 class BotsPage extends Component {
-  //start here with your code for step one
+
+constructor() {
+  super()
+  this.state = {
+    bots: []
+  }
+}
+
+  async componentDidMount() {
+   let response = await fetch('http://localhost:6001/bots')
+   let json = await response.json()
+   this.setState({
+     bots: json
+   })
+  }
 
   render() {
-    return <div>{/* put your components here */}</div>;
+    return <div>{<BotCollection bots={this.state.bots}/>}</div>;
   }
 }
 
