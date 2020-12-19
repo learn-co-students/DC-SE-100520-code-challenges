bfda9f018356f29706225bf1518fd16f4274e971
chirptotheeast <alyssa.easterly@yahoo.com>
2020-12-19 00:48:02 -0500

    got all bots done

--- BotBattlr/src/containers/BotsPage.js
+++ BotBattlr/src/containers/BotsPage.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import BotCollection from './BotCollection'
+
+const API = "http://localhost:6001/bots";
 
 class BotsPage extends Component {
   //start here with your code for step one
 
+  state = {
+    bots: [],
+  }
+
+  async componentDidMount(){
+    const response = await fetch(API);
+    const botData = await response.json();
+    this.setState({bots: botData})
+  }
+
+
   render() {
-    return <div>{/* put your components here */}</div>;
+    return <div>{<BotCollection bots={this.state.bots}/>}</div>;
   }
 }
 
